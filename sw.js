611da3b860209a7dfd832d1cf1db923f9ce4bf6a
i-rep <49623929+i-rep@users.noreply.github.com>
2019-12-10 22:07:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -62,14 +62,17 @@ var filesToCache = [
 
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-}).catch(function(err) {
-    // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+            console.log('Service worker successfully registered on scope', registration.scope);
+        }).catch(function (error) {
+            console.log('Service worker failed to register', error);
+        });
     });
 }
+
+
+
 
 
 
@@ -89,7 +92,57 @@ self.addEventListener('install', function(event) {
 
 
 
-self.addEventListener('fetch', function(event) {
+
+
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    // See /web/fundamentals/getting-started/primers/async-functions
+    // for an async/await primer.
+    event.respondWith(async function() {
+      // Optional: Normalize the incoming URL by removing query parameters.
+      // Instead of https://example.com/page?key=value,
+      // use https://example.com/page when reading and writing to the cache.
+      // For static HTML documents, it's unlikely your query parameters will
+      // affect the HTML returned. But if you do use query parameters that
+      // uniquely determine your HTML, modify this code to retain them.
+      const normalizedUrl = new URL(event.request.url);
+      normalizedUrl.search = '';
+
+      // Create promises for both the network response,
+      // and a copy of the response that can be used in the cache.
+      const fetchResponseP = fetch(normalizedUrl);
+      const fetchResponseCloneP = fetchResponseP.then(r => r.clone());
+
+      // event.waitUntil() ensures that the service worker is kept alive
+      // long enough to complete the cache update.
+      event.waitUntil(async function() {
+        const cache = await caches.open(cacheName);
+        await cache.put(normalizedUrl, await fetchResponseCloneP);
+      }());
+
+      // Prefer the cached response, falling back to the fetch response.
+      return (await caches.match(normalizedUrl)) || fetchResponseP;
+    }());
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*self.addEventListener('fetch', function(event) {
     
 
 
@@ -121,7 +174,7 @@ self.addEventListener('fetch', function(event) {
             if(response){
                 return response;
             }
-/*            else{
+            else{
                 // clone request stream
                 // as stream once consumed, can not be used again
                 var reqCopy = event.request.clone();
@@ -157,9 +210,9 @@ self.addEventListener('fetch', function(event) {
 
                     return response; // response stream consumed
                 })
-            }*/
+            }
 
-            
+
         })
     );
 
@@ -181,4 +234,4 @@ self.addEventListener('activate', function(event) {
             );
         })
     );
-});
+});*/
